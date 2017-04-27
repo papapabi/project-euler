6479013fa7bd28e132dcadd65b8b3427c50b4c41
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-04-27 03:28:57 +0000

    Problem 7 solved

---
+++ 7.rb
@@ -0,0 +1,27 @@
+require "benchmark"
+
+primes = [2] # initialize list of primes, starting with 2
+i = 3 # current number
+
+time = Benchmark.measure do
+  until primes.size == 10001 do
+    primes.each do |prime|
+      if(i % prime == 0) # if the number is divisible by the current prime, check next i immediately
+        break
+      else
+        if(prime == primes.last) #
+          primes.push(i) # ..we found a new prime!
+        else
+          next
+        end
+      end
+    end
+    i += 1
+  end
+end
+
+puts "10001th prime: #{primes.last}"
+puts "Time elapsed (in seconds): #{time}"
+
+
+
