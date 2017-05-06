88dab460a87fb3c8390c7158ad8f15488e1fe997
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-05-06 02:17:45 +0000

    Update solution for problem 15

--- 15.rb
+++ 15.rb
@@ -1,25 +1,28 @@
 require 'benchmark'
 
-
 time = Benchmark.measure do
-  @paths = 0
-  # Solves a given 2D array according to the rules of Problem #15.
-  # Paths can only move right or down, and the end goal of a path is to
-  # reach the bottom right corner.
-  #
-  # It should be noted that the Array#transpose method runs at O(m * n) time,
-  # which stupidly slow for big grids.
 
-  ar = Array.new(20) { Array.new(20, 0) }
+  # This udpate to the solution uses dynamic programming instead of recursion, which took more than 5 hours to solve.
+  COLUMNS = 20
+  ROWS = 20
 
-  def solve(ar)
-    return @paths += 1 if ar.first.length == 1
-    @paths += 1 ; solve(ar.drop(1)) if ar.length > 1 # Move downwards if we still have more than 1 row
-    @paths += 1 ; solve(ar.transpose.drop(1).transpose) if ar.transpose.length > 1 # Move rightwards if we still have more than 1 col
+  # Leverage the fact that once you hit the rightmost column, you can only go down.
+  # Likewise, for the bottommost row, you can only go right.
+  grid = Array.new(ROWS) { Array.new(COLUMNS + 1) }
+  grid.push(Array.new(COLUMNS + 1, 1))
+  grid.map! do |grid_row|
+    grid_row.pop
+    grid_row.push(1)
+  end
+
+  (ROWS-1).downto(0) do |i|
+    (COLUMNS-1).downto(0) do |j|
+      grid[i][j] = grid[i+1][j] + grid[i][j+1] # The paths to a given point is the sum of the paths immediately below and to the right of it.
+    end
   end
 
   puts "The 20x20 grid, according to the rules described by the problem statement,
-  has #{solve(ar)} paths to the bottom right corner."
+  has #{grid.first.first} paths to the bottom right corner."
 end
 
 puts "Time elapsed (in seconds): #{time}"
