7a7b70bd9f2011eaf706df25342d8a13623e3758
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-05-28 23:28:10 +0000

    Problem 39 solved

--- 39.rb
+++ 39.rb
@@ -1,3 +1,27 @@
 require 'benchmark'
+require 'bigdecimal'
 
+# Finds the perimeter p that has a maximal amount of integral solutions
+# to the pythagorean identity c^2 = a^2 + b^2.
+def max_solutions(n)
+  max_solutions = 0
+  perimeter = 0
+  1.upto(n) do |c|
+    solutions = [] # All valid integral solutions for this part. perimeter c.
+    1.upto(c-1) do |b|
+      a = BigDecimal(c/2) * BigDecimal(c - 2*b)/BigDecimal(c - b)
+      next if a.to_i != a or a <= 0 # Only integral solutions are allowed.
+      solutions << [c, b, a]
+    end
+    if solutions.length > max_solutions
+      perimeter = c
+      max_solutions = solutions.length
+    end
+  end
+  return perimeter
+end
 
+time = Benchmark.measure do
+  puts "The perimeter p<=1000 which gives the max integral solutions is #{max_solutions(1000)}"
+end
+puts "Time elapsed (in seconds): #{time}"
